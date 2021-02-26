fb4918832a58b87ae118eb92e41fff541fb6f019
Lan Chu <hoanglan.ict@gmail.com>
2021-02-26 20:37:37 +0700

    Ex 6.5

--- Lab 01/6.5/src/SortArray.java
+++ Lab 01/6.5/src/SortArray.java
@@ -1,9 +1,42 @@
 
 public class SortArray {
+	
+	static void sort(int[] arr) {
+		for(int i = 0; i < arr.length; ++i) {
+			for (int j = i+1; j < arr.length; ++j) {
+				if (arr[i] > arr[j]) {
+					int temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int[] arr = {0, 2, 4, 6, 8, 1 ,3 ,5, 7, 9};
+		int sum = 0;
+		double average = 0.0;
 		
+		System.out.println("The array: ");
+		for(int i = 0; i < arr.length; ++i)
+		{
+			System.out.print(arr[i] + " ");
+			sum += arr[i];
+		}
+		average = (double)sum / (double)arr.length;
+		
+		System.out.println("\nThe sorted array: ");
+		sort(arr);
+		for(int i = 0; i < arr.length; ++i)
+		{
+			System.out.print(arr[i] + " ");
+			sum += arr[i];
+		}
+		
+		
+		System.out.print("\nSum: " + sum + " Average: " + average);
 	}
 
 }
