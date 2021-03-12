8584cce125703e8bb5533df78b6de4c5a592a495
Lan Chu <hoanglan.ict@gmail.com>
2021-03-12 14:01:02 +0700

    First Submmit

---
+++ Lab03/AimsProject/src/Aims.java
@@ -0,0 +1,27 @@
+
+public class Aims {
+
+	public static void main(String[] args) {
+		// Create a new cart
+		Cart anOdrder = new Cart();
+		
+		// Create new DVD objects and add them to the cart
+		DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+		DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
+		DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", "Roger Allers", 18.99f);
+		
+		anOdrder.addDigitalVideoDisc(dvd1);
+		anOdrder.addDigitalVideoDisc(dvd2);
+		anOdrder.addDigitalVideoDisc(dvd3);
+		
+		// Print total cost
+		System.out.println("The total cost is: ");
+		System.out.println(anOdrder.totalCost());
+		
+		anOdrder.printCart();
+		System.out.println();
+		
+		anOdrder.removeDigitalVideoDisc(dvd3);
+	}
+
+}
---
+++ Lab03/AimsProject/src/Cart.java
@@ -0,0 +1,54 @@
+import java.util.Iterator;
+
+public class Cart {
+	public static final int MAX_NUMBERS_ORDERED = 20;
+	private DigitalVideoDisc itemsOrdered[] = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
+	int qtyOrdered;
+	
+	public void printCart() {
+		if (qtyOrdered <= 0) {
+			System.out.println("The cart is empty");
+		} else {
+			for(int i = 0; i < qtyOrdered; ++i) {
+				System.out.print(itemsOrdered[i].getTitle() + " - ");
+				System.out.print(itemsOrdered[i].getCategory() + " - ");
+				System.out.print(itemsOrdered[i].getDirector() + " - "); 
+				System.out.print(itemsOrdered[i].getLength() + " - ");
+				System.out.print(itemsOrdered[i].getCost());
+				System.out.println();
+			}
+		}
+	}
+	
+	public void addDigitalVideoDisc(DigitalVideoDisc disc) {
+		if (qtyOrdered < MAX_NUMBERS_ORDERED) {
+			itemsOrdered[qtyOrdered] = disc;
+			qtyOrdered++;
+			System.out.println("The disc has been added successfully.");
+		} else {
+			System.out.println("The cart is full. Please remove some items to add new one");
+		}
+	}
+	
+	public DigitalVideoDisc searchDVD(DigitalVideoDisc disc) {
+		for(int i = 0; i < qtyOrdered; ++i) {
+			if (itemsOrdered[i] == disc) {
+				return itemsOrdered[i];
+			}
+		}
+		return null;
+	}
+	
+	public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
+		
+	}
+	
+	public float totalCost() {
+		float total = 0;
+		for(int i=0; i<qtyOrdered; ++i) {
+			float disCost = itemsOrdered[i].getCost();
+			total += disCost;
+		}
+		return total;
+	}
+}
